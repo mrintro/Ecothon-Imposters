599b6e13f95c19508042fddff6ae29106670d569
mrintro <aniketpanwar.1998@gmail.com>
2021-11-22 13:03:44 +0530

    Optimised onFocusChanged in custom text view

--- app/src/main/java/com/example/ecothon/ui/customUIPackage/CustomEditText.kt
+++ app/src/main/java/com/example/ecothon/ui/customUIPackage/CustomEditText.kt
@@ -2,31 +2,48 @@ package com.example.ecothon.ui.customUIPackage
 
 import android.app.Activity
 import android.content.Context
-import android.content.ContextWrapper
 import android.graphics.Rect
+import android.os.Looper
 import android.util.AttributeSet
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.AppCompatEditText
-import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.content.ContextCompat.getSystemService
-import com.example.ecothon.MainActivity
-import com.example.ecothon.R
-import com.example.ecothon.Utility
+
 
 class CustomEditText @JvmOverloads constructor(
     context: Context, attributeSet: AttributeSet? = null, defStyleAttr: Int = android.R.attr.editTextStyle
 ) : AppCompatEditText(context, attributeSet,  defStyleAttr){
 
+    companion object{
+        private var isAnyTextViewFocused = false
+    }
+
     private val contextStored = context as Activity
+
+    override fun callOnClick(): Boolean {
+        return super.callOnClick()
+    }
 
 
     override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
-        Log.d("Focus", (context as Activity).currentFocus.toString())
         if(!focused) {
-            var inputMethodManager =
-                contextStored.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-            val res = inputMethodManager.hideSoftInputFromWindow(applicationWindowToken, 0)
+            isAnyTextViewFocused = false
+            Log.d("Focus Check", "false")
+            android.os.Handler(Looper.getMainLooper()).postDelayed(
+                Runnable {
+                    kotlin.run {
+                        if(!isAnyTextViewFocused){
+                            var inputMethodManager =
+                                contextStored.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                            val res = inputMethodManager.hideSoftInputFromWindow(applicationWindowToken, 0)
+                        }
+                    }
+                }, 100
+            )
+        }
+        if(focused){
+            Log.d("Focus Check", "true")
+            isAnyTextViewFocused = true
         }
         super.onFocusChanged(focused, direction, previouslyFocusedRect)
     }
