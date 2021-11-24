1190efd802a341ed82d8716ba69ad94613d8239c
mrintro <aniketpanwar.1998@gmail.com>
2021-11-24 23:45:01 +0530

    daily commit

--- app/src/main/java/com/example/ecothon/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/ecothon/ui/auth/AuthActivity.kt
@@ -2,6 +2,7 @@ package com.example.ecothon.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import androidx.viewpager2.widget.ViewPager2
 import com.example.ecothon.ui.auth.adapter.AuthViewPagerAdapter
@@ -17,6 +18,9 @@ class AuthActivity : AppCompatActivity() {
     val titles = arrayListOf<String>("Login", "Register")
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        println("Check log")
+        Log.d("Check","porint nahi hona chahiye")
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         val view = binding.root
