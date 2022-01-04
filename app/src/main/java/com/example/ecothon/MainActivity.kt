40d1f03af8f9dd0cbc331c9233e3e9f0c872c80c
mrintro <aniketpanwar.1998@gmail.com>
2022-01-04 20:11:05 +0530

    Task 1 adv android training

--- app/src/main/java/com/example/ecothon/MainActivity.kt
+++ app/src/main/java/com/example/ecothon/MainActivity.kt
@@ -6,11 +6,18 @@ import android.os.Bundle
 import android.util.Log
 import com.example.ecothon.ui.auth.AuthActivity
 import com.example.ecothon.R
+import com.example.ecothon.ui.learnings.fragmentLearning.FragmentLearningActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         finish()
-        startActivity(Intent(this, AuthActivity::class.java))
+    //  AuthActivity Intent
+    //  startActivity(Intent(this, AuthActivity::class.java))
+
+
+    //  Fragment Learning
+    startActivity(Intent(this, FragmentLearningActivity::class.java))
+
     }
 }
---
+++ app/src/main/java/com/example/ecothon/ui/learnings/fragmentLearning/FragmentLearningActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecothon.ui.learnings.fragmentLearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecothon.R
+
+class FragmentLearningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_learning)
+    }
+}
