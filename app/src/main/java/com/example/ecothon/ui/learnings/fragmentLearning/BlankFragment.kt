e6de4df53f9345a82f2ba4dc9ed6efa9cef99f32
mrintro <aniketpanwar.1998@gmail.com>
2022-01-07 22:09:55 +0530

    dependencies added

--- app/src/main/java/com/example/ecothon/ui/learnings/fragmentLearning/BlankFragment.kt
+++ app/src/main/java/com/example/ecothon/ui/learnings/fragmentLearning/BlankFragment.kt
@@ -1,13 +1,30 @@
 package com.example.ecothon.ui.learnings.fragmentLearning
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.ecothon.R
+import java.lang.ClassCastException
 
 class BlankFragment : Fragment() {
+
+    object Constants {
+        const val NONE = 2
+    }
+    var mRadioButtonChoice = Constants.NONE
+    lateinit var listener : onFragmentInteractionListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is onFragmentInteractionListener)
+            listener = context as onFragmentInteractionListener
+        else
+            throw ClassCastException(context.toString())
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,4 +32,8 @@ class BlankFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_blank, container, false)
     }
+}
+
+interface onFragmentInteractionListener {
+    fun onRadioButtonChoice(choice : Int)
 }
--- app/src/main/java/com/example/ecothon/ui/learnings/fragmentLearning/FragmentLearningActivity.kt
+++ app/src/main/java/com/example/ecothon/ui/learnings/fragmentLearning/FragmentLearningActivity.kt
@@ -3,10 +3,18 @@ package com.example.ecothon.ui.learnings.fragmentLearning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ecothon.R
+import com.example.ecothon.databinding.ActivityFragmentLearningBinding
 
 class FragmentLearningActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityFragmentLearningBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment_learning)
+        binding = ActivityFragmentLearningBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
+
 }
